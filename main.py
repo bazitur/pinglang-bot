ab7f2bfc07f0460e1c9ac61405568e8fd3781bee
Vasily <vaskathekot@gmail.com>
2021-01-21 00:25:53 +0300

    Initial HTML styling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from praw import Reddit as RedditAPI
 from telegram import Bot as TelegramBotAPI
-from flask import Flask
+from flask import Flask, render_template
 
 # app = Flask(__name__)
 
@@ -53,7 +53,7 @@ def post_something():
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
